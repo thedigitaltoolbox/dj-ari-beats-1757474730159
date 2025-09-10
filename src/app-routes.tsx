9209f9370d3aa2fbdde335fe3325b345a5b43d66
jonydigitaltoolbox <231252264+jonydigitaltoolbox@users.noreply.github.com>
2025-09-10 03:34:43 +0000

    chore: apply sitespec (Netlify will build)

--- src/app-routes.tsx
+++ src/app-routes.tsx
@@ -1,13 +1,7 @@
 import type { RouteObject } from "react-router-dom";
 import home from "./pages/home";
-import services from "./pages/services";
-import pricing from "./pages/pricing";
-import contact from "./pages/contact";
 
 const routes: RouteObject[] = [
   { path: "home", element: <home /> },
-  { path: "services", element: <services /> },
-  { path: "pricing", element: <pricing /> },
-  { path: "contact", element: <contact /> },
 ];
 export default routes;
